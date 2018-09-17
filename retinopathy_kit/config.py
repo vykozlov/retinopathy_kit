7501ea5bd66fcf1d15fda91d7fc62329341293c7
valentin.kozlov <valentin.kozlov@kit.edu>
2018-09-17 09:08:36 +0200

    initial renamings for dogs_breed to retinopathy

--- retinopathy_kit/config.py
+++ retinopathy_kit/config.py
@@ -3,3 +3,10 @@ from os import path
 
 # identify basedir for the package
 BASE_DIR = path.dirname(path.normpath(path.dirname(__file__)))
+RPKIT_Storage = 'https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/'
+RPKIT_DatasetURL = Dog_Storage + 'dogImages.zip'
+RPKIT_DataDir = 'dogImages'
+RPKIT_LabelsFile = path.join(BASE_DIR,'data','dog_names.txt')
+RPKIT_LabelsNum = 133
+
+
