51e1ee6e6db3922cc5c2453f4bbf6567d7f331f4
valentin.kozlov <valentin.kozlov@kit.edu>
2018-09-18 23:00:24 +0200

    add inceptionV3.py

---
+++ retinopathy_kit/models/inceptionV3.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 21:29:57 2018
+
+@author: valentin
+"""
+
+import retinopathy_kit.config as cfg
+import retinopathy_kit.models.general_net as gennet
+
+
+def get_metadata():
+    """
+    Simple call to get_metadata and set name to _InceptionV3:
+    """ 
+    meta = gennet.get_metadata()
+    meta['Name'] = "Retinopathy_InceptionV3"
+
+    return meta
+        
+
+def build_model():
+    """
+    Simple call to InceptionV3:
+    """  
+    return gennet.build_model('InceptionV3', cfg.RPKIT_LabelsNum)
+        
+
+def predict_file(img_path):
+    """
+    Simple call to gennet.predict_file() using InceptionV3
+    :param img_path: image to classify, full path  
+    :return: most probable label
+    """
+    return gennet.predict_file(img_path, 'InceptionV3')
+
+
+def predict_data(img):
+    """
+    Simple call to gennet.predict_data() using InceptionV3
+    """    
+    return gennet.predict_data(img, 'InceptionV3')
+
+
+def predict_url(*args):
+    """
+    Simple call to gennet.predict_url()
+    """    
+
+    return gennet.predict_url(*args)
+        
+
+def train(nepochs=10):
+    """
+    Simple call to gennet.train() using InceptionV3
+    """ 
+
+    return gennet.train(nepochs, 'InceptionV3')
